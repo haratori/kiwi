cbc51a48c1365f1079b7af5144f4ce61a2a258b4
伊原誠人 <makoto-i@makoto-i.local>
2016-05-28 08:08:31 +0900

    Initial Commit

---
+++ AiDictionary/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AiDictionary
+//
+//  Created by 伊原誠人 on 2016/05/28.
+//  Copyright © 2016年 makoto-i. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
